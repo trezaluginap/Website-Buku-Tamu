76e67590ab25a9ce73087d76e9b55a612fb5c150
faisal Az <faisalabdulaziz761@gmail.com>
2025-05-05 12:58:35 +0700

    perubahan

--- src/backend/server.js
+++ src/backend/server.js
@@ -44,8 +44,9 @@ app.post('/api/admin-login', (req, res) => {
   });
 });
 
-// Jalankan server
+// Jalankan Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
